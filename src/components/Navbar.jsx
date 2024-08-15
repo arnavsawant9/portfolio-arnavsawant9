e73bc6a1f542f662a576357aafd48950cb6e6839
arnavsawant9 <arnavsawant2004@gmail.com>
2024-08-16 00:05:28 +0530

    updated all the changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import ArticleIcon from '@mui/icons-material/Article';
+// import { Typography, Box, Stack, Link } from "@mui/material";
+
+function Navbar() {
+  return (
+    <>
+      <div className="flex justify-center items-start navbar">
+        <nav className="flex gap-6 space-x-4">
+          <img />
+          <button>Product</button>
+          <button>Features</button>
+          <button>Marketplace</button>
+          <button>Company</button>
+          <button>Login</button>
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
